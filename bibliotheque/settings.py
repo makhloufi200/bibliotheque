f8b246b331746272e9d3332b1ae0bbbc40ce31c1
makhloufi <makhloufi200@gmail.com>
2018-04-09 15:45:12 +0100

    add force

--- bibliotheque/settings.py
+++ bibliotheque/settings.py
@@ -117,17 +117,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
-#STATIC_URL = '/static/'
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(STATIC_ROOT, 'static'),
 )
