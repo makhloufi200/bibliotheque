413a2be7e43254e39708d0d98e8de5cb052ba3ff
makhloufi <makhloufi200@gmail.com>
2018-04-10 10:24:50 +0100

    Add Index Route

--- bibliotheque/urls.py
+++ bibliotheque/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from biblio import views
 urlpatterns = [
+    path('', views.biblio),
     path('admin', admin.site.urls),
     path(r'biblio',include('biblio.urls')),
     path(r'search', views.search)
